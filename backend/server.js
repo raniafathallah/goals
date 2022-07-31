be0fc65cac4b18cd26e398a5f90ad88043271307
Rania <raniaahmed7000@gmail.com>
2022-07-31 02:40:42 +0200

    rest api

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express=require('express')
+const colors=require('colors')
+const dotenv=require('dotenv').config()
+
+//const {errorHandler}=require('./middleware/errorMiddleware')
+
+
+const connectDB=require('./config/db');
+connectDB();
+const app =express();
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/api/goals',require('./routes/goalRoutes'))
+
+
+app.listen(process.env.PORT, () => console.log(`Server started on port ${process.env.PORT}`))
